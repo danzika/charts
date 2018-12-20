d72f7854eb76774f2d39ab38ba9cf53dd521e81b
Jan Soubusta <jan.soubusta@gooddata.com>
2018-12-20 18:05:57 +0100

    improve deploy

--- stable/minio/deploy_ii.sh
+++ stable/minio/deploy_ii.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# clusterExec.py -m ii-k8s-worker{01..04} -- 'sudo mkdir /mnt/minio1 /mnt/minio2'
 
-kubectl create -f gdc-pv-ii.yaml
+if [ "`whoami`" != "root" ]; then
+    echo "ERROR: you must be root to run this script!"
+    exit 1
+fi
+
+kubectl create -f stable/minio/gdc-pv-ii.yaml
 
 helm install stable/minio/ --name minio-cluster-1 -f stable/minio/gdc-values-ii.yaml
 helm install stable/minio/ --name minio-cluster-2 -f stable/minio/gdc-values-ii.yaml
--- stable/minio/deploy_perf.sh
+++ stable/minio/deploy_perf.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+# clusterExec.py -m perf-k8s-worker{01..04} -- 'sudo mkdir /mnt/minio1 /mnt/minio2'
 
-kubectl create -f gdc-pv.yaml
+if [ "`whoami`" != "root" ]; then
+    echo "ERROR: you must be root to run this script!"
+    exit 1
+fi
+
+kubectl create -f stable/minio/gdc-pv-perf.yaml
 
 helm install stable/minio/ --name minio-cluster-1 -f stable/minio/gdc-values-perf.yaml
+helm install stable/minio/ --name minio-cluster-2 -f stable/minio/gdc-values-perf.yaml
