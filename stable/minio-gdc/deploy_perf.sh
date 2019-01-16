1da2ced01bae67c3c016d45907e4f7eaa7406878
Jan Soubusta <jan.soubusta@gooddata.com>
2019-01-16 16:48:42 +0100

    cleanup

--- stable/minio-gdc/deploy_perf.sh
+++ stable/minio-gdc/deploy_perf.sh
@@ -1,37 +1,33 @@
 #!/usr/bin/env bash
 
 
-kubectl create -f ../minio/gdc-pv-perf.yaml
+kubectl create -f gdc-pv-perf.yaml
 
 ###############################################################3
 # Cluster 1
 ###############################################################3
 
 # Without federation
-helm install stable/minio/ --name minio-cluster-1 \
-  --set service.nodePort=32080 \
-  -f ../minio/gdc-values.yaml
+helm install ../minio/ --name minio-cluster-1 -f gdc-values.yaml \
+  --set service.nodePort=32080
 
 # Upgrade example
-helm upgrade minio-cluster-1 stable/minio/ \
-  --set service.nodePort=32080 \
-  -f ../minio/gdc-values.yaml
+helm upgrade minio-cluster-1 ../minio/ -f gdc-values.yaml \
+  --set service.nodePort=32080
 
 # With federation
-helm install stable/minio/ --name minio-cluster-1 \
+helm install ../minio/ --name minio-cluster-1 -f gdc-values.yaml \
   --set service.nodePort=32080 \
   --set environment.MINIO_ETCD_ENDPOINTS=http://perf-k8s-master01.int.na.prodgdc.com:2379 \
   --set environment.MINIO_DOMAIN=minio.k8s.gdc.com \
-  --set environment.MINIO_PUBLIC_IPS=minio-cluster-1 \
-  -f ../minio/gdc-values.yaml
+  --set environment.MINIO_PUBLIC_IPS=minio-cluster-1
 
 # Upgrade example
-helm upgrade minio-cluster-1 stable/minio/ \
+helm upgrade minio-cluster-1 ../minio/ -f gdc-values.yaml \
   --set service.nodePort=32080 \
   --set environment.MINIO_ETCD_ENDPOINTS=http://perf-k8s-master01.int.na.prodgdc.com:2379 \
   --set environment.MINIO_DOMAIN=minio.k8s.gdc.com \
-  --set environment.MINIO_PUBLIC_IPS=minio-cluster-1 \
-  -f ../minio/gdc-values.yaml
+  --set environment.MINIO_PUBLIC_IPS=minio-cluster-1
 
 
 ###############################################################3
@@ -39,20 +35,18 @@ helm upgrade minio-cluster-1 stable/minio/ \
 ###############################################################3
 
 # Without federation
-helm install stable/minio/ --name minio-cluster-2 \
+helm install ../minio/ --name minio-cluster-2 -f gdc-values.yaml \
   --set service.nodePort=32081 \
-  -f ../minio/gdc-values.yaml
 
 # With federation
-helm install stable/minio/ --name minio-cluster-2 \
+helm install ../minio/ --name minio-cluster-2 -f gdc-values.yaml \
   --set service.nodePort=32081 \
   --set environment.MINIO_ETCD_ENDPOINTS=http://perf-k8s-master01.int.na.prodgdc.com:2379 \
   --set environment.MINIO_DOMAIN=minio.k8s.gdc.com
-  -f ../minio/gdc-values.yaml
 
 
 # TODO - deploy Ingress with nginx controller like in Hackaton
 # Use same port for all Minio clusters
 # Consider to (do not) use federation mode
 
-kubectl create -f ../minio/temp_ingress_nginx.yaml
+kubectl create -f temp_ingress_nginx.yaml
--- stable/minio-gdc/destroy_perf.sh
+++ stable/minio-gdc/destroy_perf.sh
@@ -16,6 +16,6 @@ kubectl delete pvc export-minio-cluster-2-1
 kubectl delete pvc export-minio-cluster-2-2
 kubectl delete pvc export-minio-cluster-2-3
 
-kubectl delete -f ../minio/gdc-pv-perf.yaml
+kubectl delete -f gdc-pv-perf.yaml
 
 clusterExec.py -m perf-k8s-worker{01..08} -- 'sudo rm -rf /mnt/minio1'
