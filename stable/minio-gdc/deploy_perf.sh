d49a72f1bad0ec8b3c99475a64e735020409442a
Jan Soubusta <jan.soubusta@gooddata.com>
2019-03-13 12:07:37 +0100

    value file per cluster

--- stable/minio-gdc/deploy_perf.sh
+++ stable/minio-gdc/deploy_perf.sh
@@ -5,10 +5,14 @@ export NAMESPACE=minio
 export NODE_PORT=32080
 export SERVICE_PORT=9000
 
+kubectl create --namespace ${NAMESPACE} -f gdc-pv-perf.yaml
+
+helm install ../minio/ --name minio-4node-200G-c01 --namespace ${NAMESPACE} -f minio-4node-200G-c01.yaml
+helm install ../minio/ --name minio-4node-200G-c01 --namespace ${NAMESPACE} -f minio-4node-200G-c01.yaml
+
+
 export CLUSTER_NAME="minio-cluster-1"
 export CLUSTER_NAME="minio-cluster-2"
-
-kubectl create --namespace ${NAMESPACE} -f gdc-pv-perf.yaml
 
 # Without federation, NodePort - DEPRECATED
 helm install ../minio/ --name ${CLUSTER_NAME} --namespace ${NAMESPACE} -f gdc-values.yaml \
@@ -33,6 +37,28 @@ helm upgrade ${CLUSTER_NAME} ../minio/ --namespace ${NAMESPACE} -f gdc-values.ya
   --set ingress.enabled=true --set ingress.path=/
 
 # TODO - how to specify path rule for each Vertica?
+
+#######################################################################################
+# Actual deployment
+export NAMESPACE=minio
+export SERVICE_PORT=9000
+
+helm install ../minio/ --name minio-cluster-1 --namespace ${NAMESPACE} -f gdc-values.yaml \
+  --set service.type=ClusterIP --set service.port=${SERVICE_PORT} \
+  --set ingress.enabled=true \
+  --set ingress.hosts="[perf-dss-v03.minio.k8s.gooddata,perf-dss-v04.minio.k8s.gooddata]"
+
+# Upgrade
+helm upgrade minio-cluster-1 ../minio/ --namespace ${NAMESPACE} -f gdc-values.yaml \
+  --set service.type=ClusterIP --set service.port=${SERVICE_PORT} \
+  --set ingress.enabled=true \
+  --set ingress.hosts="[perf-dss-v03.minio.k8s.gooddata,perf-dss-v04.minio.k8s.gooddata]"
+
+helm install ../minio/ --name minio-cluster-2 --namespace ${NAMESPACE} -f gdc-values.yaml \
+  --set service.type=ClusterIP --set service.port=${SERVICE_PORT} \
+  --set ingress.enabled=true --set ingress.hosts="[perf-dss-v03.minio.k8s.gooddata,perf-dss-v04.minio.k8s.gooddata]"
+
+
 
 # With federation, NodePort
 helm install ../minio/ --name ${CLUSTER_NAME} --namespace ${NAMESPACE} -f gdc-values.yaml \
